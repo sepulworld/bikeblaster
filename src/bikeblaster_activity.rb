58b8effe722b5eaf5dd833202b668aedc9488000
zane <zane@mochimedia.com>
2013-12-26 19:22:43 -0800

    adding audio interface to test

--- src/bikeblaster_activity.rb
+++ src/bikeblaster_activity.rb
@@ -1,11 +1,35 @@
 require 'ruboto/widget'
 require 'ruboto/util/toast'
 
+java_import 'android.media.MediaPlayer'
+java_import 'android.media.AudioManager'
+java_import 'android.net.Uri'
 ruboto_import_widgets :Button, :LinearLayout, :TextView
 
 class BikeblasterActivity
   def onCreate(bundle)
     super
+    
+    java_file = java.io.File.new('/mnt/sdcard/jruby/media/MissleLaunch.mp3')
+    uri = Uri.fromFile(java_file)
+    @player = MediaPlayer.create(self, uri)
+
+    @listener = proc { AudioFocus.new
+    context = getApplicationContext
+    audio_manager = context.getSystemService(Context::AUDIO_SERVICE)
+
+    audio_manager.requestAudioFocus(@listener, AudioManger::STREAM_MUSIC, AudioManager::AUDIOFOCUS_GAIN)
+    @player.start }
+
+  rescue Exception => e
+    puts "#{ e } (#{ e.class } #{e.message} #{e.backtrace.inspect} )!"
+      end
+  
+  def on_touch_event(event)
+    @player.stop
+    true
+  end
+    
     set_title 'Bike Blaster!'
 
     self.content_view =
@@ -32,4 +56,15 @@ class BikeblasterActivity
     toast 'Reloading...'
   end
     
+end
+
+class AudioFocus
+  def onAudioFocusChange focusChange
+    puts "On focus change #{focusChange}"
+    nil
+  end
+
+  def toString
+    self.class.to_s
+  end
 end
